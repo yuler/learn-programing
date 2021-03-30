a24cced9fbf42406265d8c46edcbbb842d20426a
yuler <joyful4yu@gmail.com>
2021-03-30 21:14:43 +0800

    [shell]: Add `07_dictionary.sh`

---
+++ shell/07_dictionary.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# refs: https://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
+
+# Defining
+# declares sound as a Dictionary object (aka associative array).
+declare -A sounds 
+# set value
+sounds[dog]="bark" 
+sounds[cow]="moo"
+sounds[bird]="tweet"
+sounds[wolf]="howl"
+
+# Working with dictionaries
+echo ${sounds[dog]}     #=> Dog's sound
+echo ${sounds[@]}       #=> All values
+echo ${!sounds[@]}      #=> All keys
+echo ${#sounds[@]}      #=> Number of elements
+unset sounds[dog]       #=> Delete dog
+
+# Iteration
+for key in ${!sounds[@]} ; do
+  echo $key
+done
+
+for val in ${sounds[@]} ; do
+  echo $val
+done
