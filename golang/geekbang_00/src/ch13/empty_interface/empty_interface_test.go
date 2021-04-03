be4d5b5b6bb5380cfee072f364c35a7474aacd21
yuler <joyful4yu@gmail.com>
2021-04-04 01:26:46 +0800

    [golang]: geekbang_00 ch13

---
+++ golang/geekbang_00/src/ch13/empty_interface/empty_interface_test.go
@@ -0,0 +1,34 @@
+package empty_interface_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func DoSomething(p interface{}) {
+	// if i, ok := p.(int); ok {
+	// 	fmt.Println("Integer", i)
+	// 	return
+	// }
+	// if s, ok := p.(string); ok {
+	// 	fmt.Println("String", s)
+	// 	return
+	// }
+	// fmt.Println("Unkown Type")
+
+	switch v := p.(type) {
+	case int:
+		fmt.Println("Integer", v)
+	case string:
+		fmt.Println("String", v)
+	default:
+		fmt.Println("Unkown Type")
+	}
+
+}
+
+func TestEmptyInterfaceAssert(t *testing.T) {
+	DoSomething(10)
+	DoSomething("10")
+	DoSomething(10.10)
+}
